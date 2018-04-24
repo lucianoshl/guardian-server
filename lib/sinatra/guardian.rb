b9d4affd1f47fe1d7a728564f87b38fc8bb1a093
Luciano Lopes <lucianoshl@gmail.com>
2018-04-24 19:36:55 -0300

    fix heroku

--- lib/sinatra/guardian.rb
+++ lib/sinatra/guardian.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'sinatra/base'
-require 'sinatra/json'
+# require 'sinatra/base'
+# require 'sinatra/json'
 
 class GuardianSinatraApp < Sinatra::Base
   get '/' do
