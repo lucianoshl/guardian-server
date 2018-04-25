f7acf894469d275cc5f530c354de7f720b04c3eb
Luciano Lopes <lucianoshl@gmail.com>
2018-04-25 08:50:22 -0300

    fix sinatra

--- lib/sinatra/guardian.rb
+++ lib/sinatra/guardian.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'sinatra/base'
-require 'sinatra/json'
+# require 'sinatra/base'
+# require 'sinatra/json'
 
 class GuardianSinatraApp < Sinatra::Base
   get '/' do
