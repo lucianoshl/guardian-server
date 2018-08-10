f238420a0da369e56035d5afd567c53e66ee5163
Luciano Lopes <lucianoshl@gmail.com>
2018-08-10 11:29:35 -0300

    better logs + fix login issues

---
+++ lib/util/logging.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'logger'
+
+module Logging
+  class << self
+    def logger
+      @logger ||= Logger.new($stdout)
+      @logger.formatter = proc do |_severity, _datetime, _progname, msg|
+        "#{msg}\n"
+      end
+      @logger
+    end
+
+    attr_writer :logger
+  end
+
+  # Addition
+  def self.included(base)
+    class << base
+      def logger
+        Logging.logger
+      end
+    end
+  end
+
+  def logger
+    Logging.logger
+  end
+end
