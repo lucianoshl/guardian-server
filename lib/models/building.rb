6ca27cfced17562479354292e7c9da3cd413a490
Luciano Lopes <lucianoshl@gmail.com>
2018-08-19 03:26:16 -0300

    fix merge error

--- lib/models/building.rb
+++ lib/models/building.rb
@@ -1,10 +1,7 @@
 class Building
   include Mongoid::Document
   field :name, type: String
-<<<<<<< HEAD
-=======
   field :label, type: String
->>>>>>> origin/partial-work
 
   field :max_level, type: Integer
   field :min_level, type: Integer
@@ -36,11 +33,7 @@ class Building
     (attributes["pop"] * pop_factor ** (level - 1)).round
   end
 
-<<<<<<< HEAD
-=======
   def self.ids
     Building.all.pluck(:id)
   end
-
->>>>>>> origin/partial-work
 end
