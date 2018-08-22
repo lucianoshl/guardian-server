b475d2512cb19e08c0bfb433d4c2753136b010ac
Luciano Lopes <lucianoshl@gmail.com>
2018-08-22 16:22:45 -0300

    logs

--- lib/models/task/abstract.rb
+++ lib/models/task/abstract.rb
@@ -30,7 +30,7 @@ class Task::Abstract
 
   after_save do
     schedule unless next_execution.nil?
-    log.debug("Task Saved: #{attributes.json}")
+    logger.debug("Task Saved: #{attributes.json}")
   end
 
   def execute
