e909fba68f2520a4b78f9954cfc830f03ccae7b5
Luciano Lopes <lucianoshl@gmail.com>
2018-08-29 16:50:37 -0300

    remove locks before execution

--- bin/delayed_job.rb
+++ bin/delayed_job.rb
@@ -10,4 +10,12 @@ Delayed::Worker.logger = Logger.new(STDOUT)
 Delayed::Worker.logger.level = -1
 
 Delayed::Worker.backend = :mongoid
+
+queue_name = ARGV.select{|a| a =~ /--queue=/}.first.scan(/--queue=(.+)/).first.first
+
+Delayed::Backend::Mongoid::Job.where(queue: queue_name).map do |job|
+  job.unlock
+  job.save
+end
+
 Delayed::Command.new(ARGV).daemonize
