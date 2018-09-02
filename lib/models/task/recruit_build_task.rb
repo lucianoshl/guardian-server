bf92f49302154b02e141e47623750b37ba9bea75
Luciano Lopes <lucianoshl@gmail.com>
2018-09-01 22:07:03 -0300

    auto build

--- lib/models/task/recruit_build_task.rb
+++ lib/models/task/recruit_build_task.rb
@@ -118,10 +118,10 @@ class Task::RecruitBuildTask < Task::Abstract
   runs_every 30.minutes
 
   def run
-    Account.main.player.villages.map do |village|
+    results = Account.main.player.villages.map do |village|
       run_for_village(village)
     end
-    nil
+    results.compact.sort.first || nil
   end
 
   def run_for_village(village)
