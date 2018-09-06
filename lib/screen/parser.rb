e727e6a694025f152fde747e9b0b0839b2b8182c
Luciano Lopes <lucianoshl@gmail.com>
2018-09-06 19:51:35 -0300

    fix bug

--- lib/screen/parser.rb
+++ lib/screen/parser.rb
@@ -6,7 +6,7 @@ module Screen::Parser
   end
 
   def parse_table(page, selector, remove_columns: [], include_header: false)
-    generate_table_selector.map_compact do |tr|
+    generate_table_selector(page,selector).map_compact do |tr|
       if tr.search('th').empty? || include_header
         tr.search('td').select_index(remove_columns).map(&:remove) unless remove_columns.empty?
         tr
@@ -14,11 +14,11 @@ module Screen::Parser
     end
   end
 
-  def generate_table_selector(base_selector)
+  def generate_table_selector(page,base_selector)
     has_thead = !page.search("#{base_selector} > thead").empty?
     has_tbody = !page.search("#{base_selector} > tbody").empty?
     if has_thead || has_tbody
-      page.search("#{base_selector} > thead > tr, #{selector} > tbody > tr")
+      page.search("#{base_selector} > thead > tr, #{base_selector} > tbody > tr")
     else
       page.search("#{base_selector} > tr")
     end
