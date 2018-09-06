086a26306665e7228e238e5fd232a5e43c01bc02
Luciano Lopes <lucianoshl@gmail.com>
2018-09-06 19:47:16 -0300

    fix cognitive complexity

--- lib/screen/parser.rb
+++ lib/screen/parser.rb
@@ -6,21 +6,21 @@ module Screen::Parser
   end
 
   def parse_table(page, selector, remove_columns: [], include_header: false)
-    tr_list = []
-    has_thead = !page.search("#{selector} > thead").empty?
-    has_tbody = !page.search("#{selector} > tbody").empty?
-
-    tr_list = if has_thead || has_tbody
-                page.search("#{selector} > thead > tr, #{selector} > tbody > tr")
-              else
-                page.search("#{selector} > tr")
-              end
-
-    tr_list.map_compact do |tr|
+    generate_table_selector.map_compact do |tr|
       if tr.search('th').empty? || include_header
         tr.search('td').select_index(remove_columns).map(&:remove) unless remove_columns.empty?
         tr
       end
+    end
+  end
+
+  def generate_table_selector(base_selector)
+    has_thead = !page.search("#{base_selector} > thead").empty?
+    has_tbody = !page.search("#{base_selector} > tbody").empty?
+    if has_thead || has_tbody
+      page.search("#{base_selector} > thead > tr, #{selector} > tbody > tr")
+    else
+      page.search("#{base_selector} > tr")
     end
   end
 end
