4505180f28b37c7b69befa9dfa90676f1c9ad951
Luciano Lopes <lucianoshl@gmail.com>
2018-10-03 15:54:54 -0300

    fix graphql model

---
+++ lib/graphql_model/type/troop_model.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Type::TroopModel
+  include Type::Base
+end
---
+++ lib/graphql_model/type/village_model.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Type::VillageModel
+  include Type::Base
+end
