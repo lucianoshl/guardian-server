031caae75c5ada5db99e6264827f905600091d25
Luciano Lopes <lucianoshl@gmail.com>
2018-12-23 18:49:37 -0200

    fix operation

--- lib/graphql_model/type/task_abstract.rb
+++ lib/graphql_model/type/task_abstract.rb
@@ -3,4 +3,15 @@
 module Type::TaskAbstract
   include Type::Base
   class_base Task::Abstract
+
+  mutation do
+    name 'RunTaskNow'
+    argument :id, !types.String
+    type Type::Village.definition
+    def call(_object, inputs, _ctx)
+      task = Task::Abstract.find(id: inputs['id'])
+      task.run_now
+      task
+    end
+  end
 end
