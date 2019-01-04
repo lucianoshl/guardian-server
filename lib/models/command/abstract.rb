e78ee93262496daf4fed74a6c0880428d1a6f46e
Luciano Lopes <lucianoshl@gmail.com>
2019-01-04 13:53:27 -0200

    git revert --no-commit faf1a8a04c296a871a5f788b4bca9b25e96bb2d4

--- lib/models/command/abstract.rb
+++ lib/models/command/abstract.rb
@@ -7,6 +7,5 @@ class Command::Abstract
   field :create_at, type: Time
 
   belongs_to :origin, class_name: Village.to_s
-  belongs_to :target_village, class_name: Village.to_s
   embeds_one :target, class_name: Coordinate.to_s
 end
