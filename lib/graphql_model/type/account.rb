c93df4adc6dcbcbaca5813e664a28ae317de00eb
Luciano Lopes <luciano.lopes@eticasolucoes.com.br>
2019-06-06 17:11:31 -0300

    mutation

--- lib/graphql_model/type/account.rb
+++ lib/graphql_model/type/account.rb
@@ -6,4 +6,22 @@ module Type::Account
   criteria do |base|
     base.without(:password)
   end
+
+  mutation do
+    name 'CreateAccount'
+    argument :username, !types.String
+    argument :password, !types.String
+    argument :world, !types.String
+    type Type::Account.definition
+
+    def call(_object, inputs, _ctx)
+      return Account.main unless Account.main.nil?
+      new_account = Account.new(inputs.to_h)
+      new_account.main = true
+      new_account.save
+      Event::FirstLogin.new
+      new_account
+    end
+  end
+
 end
