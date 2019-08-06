1f584afe58dd511713c1619cceb9d0e1bf4b96c1
Luciano Lopes <lucianoshl@gmail.com>
2019-08-06 15:47:47 -0300

    prepare to rails

--- lib/sinatra/routes/overlay.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Routes::Overlay
-  def fix_html(page)
-    raw = page.body
-    raw = raw.gsub(/game\.php/, 'overlay')
-    raw
-  end
-end
--- lib/sinatra/web_app.rb
+++ lib/sinatra/web_app.rb
@@ -3,21 +3,34 @@
 require 'sinatra'
 
 class WebApp < Sinatra::Base
-  Helpers.constants.map do |item|
-    helpers Helpers.const_get(item)
+
+  post '/graphql' do
+    params = JSON.parse request.body.read
+    result = GuardianSchema.execute(
+      params['query'],
+      variables: params['variables'],
+      context: { current_user: nil }
+    )
+    content_type :json
+    result.to_json
   end
 
-  Routes.constants.map do |item|
-    register Routes.const_get(item)
+  get '/graphiql' do
+    Rack::GraphiQL.new(endpoint: '/graphql').call(request.env)
   end
 
-  after do
-    before_write_response(response)
+  get '/healthcheck' do
+    content_type :json
+
+    Report.where('_type': 'Troop').delete
+    Task::Abstract.remove_orphan_tasks
+
+    errors = Service::HealthCheck.check_system
+    status errors.empty? ? 200 : 500
+    body errors.to_json
+  rescue StandardError => e
+    status 500
+    raise e
   end
 
-  get '/reset' do
-    Mongoid.purge!
-    Helpers::RequestProxy.client.cookie_jar.clear
-    return redirect '/'
-  end
 end
