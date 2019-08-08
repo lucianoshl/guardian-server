e832e7196cfbbf428bc732eebc3bccd7af63496e
Luciano Lopes <lucianoshl@gmail.com>
2019-08-08 12:51:04 -0300

    health_check uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+
   post 'graphql', to: 'application#graphql'
+  get 'healthcheck', to: 'application#health_check'
 end
