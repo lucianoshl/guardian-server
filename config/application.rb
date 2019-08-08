91831ae8b366780cd06a1bcb0e50b091868e2b7e
Luciano Lopes <lucianoshl@gmail.com>
2019-08-08 11:49:51 -0300

    fix logs

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,6 @@ module Guardian
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-  Mongoid.logger = Logger.new(STDOUT)
-  Mongoid.logger.level = -1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -37,5 +34,9 @@ module Guardian
     config.autoload_paths += Dir["#{config.root}/app/{models,extras,util}/**/"]
 
     config.time_zone = 'America/Sao_Paulo'
+
+    config.mongoid.logger = Logger.new(Rails.root + '/log/mongoid.log', :warn)
+    config.mongoid.logger.level = Logger::INFO
+
   end
 end
