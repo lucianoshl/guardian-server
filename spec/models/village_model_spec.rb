a288c73a77c1e72fec2581a049b64d06b8f4aa0c
Luciano Lopes <lucianoshl@gmail.com>
2019-08-22 12:26:22 -0300

    predefined models

--- spec/models/village_model_spec.rb
+++ spec/models/village_model_spec.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 describe VillageModel do
+  it 'read models from config file' do
+    configs = VillageModel.yaml_configs
+    expect(configs.size).to eq(4)
+  end
+
+  it 'check basic model' do
+    expect(Village.basic_model.name).to eq('FIRST_VILLAGE')
+  end
 end
