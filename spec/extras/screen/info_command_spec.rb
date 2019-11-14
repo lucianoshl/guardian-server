9796d91091ac289ea0ebd1a19b3d3f1f79efbe46
Luciano Lopes <lucianoshl@gmail.com>
2019-11-14 15:18:53 -0300

    rubocop

--- spec/extras/screen/info_command_spec.rb
+++ spec/extras/screen/info_command_spec.rb
@@ -3,7 +3,7 @@
 describe Screen::InfoCommand do
   it 'load info player command' do
     mock_request_from_id('player_command')
-    screen = Screen::InfoCommand.new(id: 506937843)
+    screen = Screen::InfoCommand.new(id: 506_937_843)
     command = screen.command
     expect(command).to be_nil
   end
--- spec/models/task/player_monitoring_task_spec.rb
+++ spec/models/task/player_monitoring_task_spec.rb
@@ -6,14 +6,13 @@ describe Task::PlayerMonitoringTask do
   it 'just run twice' do
     point = Coordinate.new(x: 500, y: 500)
     result = Service::Map.find_nearby([point], 50).values
-    result.map{|a| a.distance = point.distance(a)}
+    result.map { |a| a.distance = point.distance(a) }
 
-    point = result.sort_by(&:distance).first
-    allow(Account).to receive_message_chain(:main,:player,:villages).and_return([
-      Village.new(x: point.x, y: point.y )
+    point = result.min_by(&:distance)
+    allow(Account).to receive_message_chain(:main, :player, :villages).and_return([
+      Village.new(x: point.x, y: point.y)
     ])
     subject.run
     subject.run
   end
-
 end
