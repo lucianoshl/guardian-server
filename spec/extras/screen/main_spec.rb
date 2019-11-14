b8d7ab12629f559ceaa873b205abf0db3a5c0201
Luciano Lopes <lucianoshl@gmail.com>
2019-11-14 14:41:10 -0300

    time zone issues

--- spec/extras/screen/main_spec.rb
+++ spec/extras/screen/main_spec.rb
@@ -6,6 +6,7 @@ describe do
     server_time = main.server_time
     current_time = Time.now
     diference = (current_time - server_time).abs
+    diference -= 3600 if diference > 3599
     expect(diference).to be < 500
 
     main.possible_build?(:farm)
