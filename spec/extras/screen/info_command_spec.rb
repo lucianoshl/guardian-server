33e4e560684f21554c925e250309ba3a372c3a67
Luciano Lopes <lucianoshl@gmail.com>
2019-11-14 15:16:22 -0300

    info command spec

--- spec/extras/screen/info_command_spec.rb
+++ spec/extras/screen/info_command_spec.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 describe Screen::InfoCommand do
+  it 'load info player command' do
+    mock_request_from_id('player_command')
+    screen = Screen::InfoCommand.new(id: 506937843)
+    command = screen.command
+    expect(command).to be_nil
+  end
 end
