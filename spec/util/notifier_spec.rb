1c3105181ce0fc7d76f6a9ae595295868792fdd4
Luciano Lopes <lucianoshl@gmail.com>
2019-11-22 17:49:45 -0300

    notification spec

--- spec/util/notifier_spec.rb
+++ spec/util/notifier_spec.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 describe Notifier do
+  subject { Class.new { extend Notifier } }
+
+  it 'send_notification' do
+    allow(ENV).to receive(:[]).and_return('token')
+    subject.notify('hello world')
+  end
+
+  it 'send notification with network error' do
+    allow(ENV).to receive(:[]).and_return('token')
+    allow_any_instance_of(Washbullet::Client).to receive(:push_note).and_raise(StandardError.new)
+    subject.notify('hello world')
+  end
 end
