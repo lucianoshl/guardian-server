47f6d13e171c8ef454d1d6c5ef3b2098b53063cc
Luciano Lopes <lucianoshl@gmail.com>
2019-11-22 18:00:36 -0300

    world config spec

--- spec/extras/service/world_config_spec.rb
+++ spec/extras/service/world_config_spec.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 describe Service::WorldConfig do
+  subject { Service::WorldConfig }
+
+  it 'world has miliseconds' do
+    expect(subject.has_milliseconds?).to eq(true)
+  end
+
+  it 'world without miliseconds information' do
+    allow(Service::WorldConfig).to receive(:parse_table).and_return([])
+    expect { subject.has_milliseconds? }.to raise_error('needs implementation')
+  end
+
 end
