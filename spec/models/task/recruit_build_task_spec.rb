b48d02679c3a419ba6200b0f38f96efa01f65ede
Luciano Lopes <lucianoshl@gmail.com>
2019-11-23 22:25:29 -0300

    recruit build task spec

--- spec/models/task/recruit_build_task_spec.rb
+++ spec/models/task/recruit_build_task_spec.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-# # frozen_string_literal: true
+describe Task::RecruitBuildTask do
 
-# describe Task::RecruitBuildTask do
+  subject { Task::RecruitBuildTask.new }
 
-#   it 'just run' do
-#     Task::RecruitBuildTask.new.run
-#   end
+  it 'just run with recruit disabled' do
+    allow(Account.main).to receive_message_chain(:player, :villages).and_return [
+      village = stub_village('my_001')
+    ]
 
-# end
+    allow(subject).to receive(:build).and_return(Time.now)
+    allow(village).to receive(:disable_recruit).and_return(true)
+    allow(village).to receive(:disable_build).and_return(true)
+    subject.run
+  end
+
+end
