5670cdb1ab1f006f6e0374bbd24e56a9d97f2837
Luciano Lopes <lucianoshl@gmail.com>
2019-11-23 22:19:36 -0300

    coordinate spec

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -7,7 +7,6 @@ class Coordinate
   def ==(other)
     other_methods = other.methods
     return x == other.x && y == other.y if other_methods.include?(:x) && other_methods.include?(:y)
-
     false
   end
 
--- spec/models/coordinate_spec.rb
+++ spec/models/coordinate_spec.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 describe Coordinate do
+
+  it '==' do
+    coord = Coordinate.new(x: 1, y: 1)
+    coord2 = Coordinate.new(x: 1, y: 2)
+    expect(coord == coord2).to eq(false)
+    expect(coord == coord.clone).to eq(true)
+    expect(coord == 1).to eq(false)
+  end
+
+  it 'to_s' do
+    coord = Coordinate.new(x: 1, y: 1)
+    expect(coord.to_s).to eq("1|1")
+  end
+
+  it 'to_h' do
+    coord = Coordinate.new(x: 1, y: 1)
+    coord = coord.to_h
+    expect(coord['x']).to eq(1)
+    expect(coord['y']).to eq(1)
+  end
+
 end
