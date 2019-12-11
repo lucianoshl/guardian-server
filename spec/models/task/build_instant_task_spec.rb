8f7912f8915e44fa996cff1edadbae56ec492b7c
Luciano Lopes <lucianoshl@gmail.com>
2019-12-11 16:31:01 -0300

    build instant  spec

--- spec/models/task/build_instant_task_spec.rb
+++ spec/models/task/build_instant_task_spec.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 describe Task::BuildInstantTask do
+  subject { Task::BuildInstantTask.new }
+
+  it 'test instant build' do
+    main_screen = double(:main)
+    allow(Screen::Main).to receive(:new).and_return main_screen
+    allow(main_screen).to receive(:build_instant).and_return nil
+    allow(main_screen).to receive(:reload).and_return nil
+
+    allow(subject).to receive_message_chain(:village, :id).and_return(1)
+    allow(subject).to receive(:build).and_return nil
+
+    subject.run
+    # expect(main_screen).to receive(:build_instant)
+    # expect(subject).to receive(:build)
+  end
 end
