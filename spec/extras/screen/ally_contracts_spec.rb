67e35c3db7038c4378b7dd13e832b280ececc47f
Luciano Lopes <lucianoshl@gmail.com>
2020-01-20 17:10:19 -0300

    ally contracts spec

--- spec/extras/screen/ally_contracts_spec.rb
+++ spec/extras/screen/ally_contracts_spec.rb
@@ -2,6 +2,8 @@
 
 describe Screen::AllyContracts do
   it 'do_request' do
+    mock_request_from_id('ally_contracts')
+    allow(Screen::AllyContracts).to receive(:new).and_call_original
     Screen::AllyContracts.new
   end
 end
